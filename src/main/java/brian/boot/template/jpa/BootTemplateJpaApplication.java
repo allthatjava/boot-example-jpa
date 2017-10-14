e3ed07576544da45a772f5aa618e81028c2e0b00
allthatjava <allthatjava@yahoo.ca>
2017-10-14 16:06:03 -0400

    Initial commit

---
+++ src/main/java/brian/boot/template/jpa/BootTemplateJpaApplication.java
@@ -0,0 +1,12 @@
+package brian.boot.template.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootTemplateJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootTemplateJpaApplication.class, args);
+	}
+}
